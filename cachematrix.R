b24257d76c8b339edc48ded598b4194e31f53d30
uma1989 <uma1989@gmail.com>
2015-07-26 16:58:19 +0530

    Coursera Assignment 2
    Submission of Coursera Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script contains a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## The "makeCacheMatrix" function creates a special "matrix" object
+## that stores values in order to cache its inverse.
+## The Output for the "makeCacheMatrix" function is a List of 4 functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(givenmatrix) {
+    ## Sets the value to the given matrix for which 
+    ## the inverse is to be computed
+    x <<- givenmatrix
+    i <<- NULL
+  }
+  get <- function() x
+  ## Returns the Set matrix value
+  
+  setinv <- function(givenmatrixinv) i <<- givenmatrixinv
+  ## Sets the value to the given matrix inverse
+  getinv <- function() i
+  ## Returns the Set matrix inverse value
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  ## Returns a List containing the 4 above-defined functions
 }
 
 
-## Write a short comment describing this function
+## The "cacheSolve" function computes the inverse of
+## the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix 
+## has not changed), then the "cacheSolve" function should 
+## retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(specialmatrix, ...) {
+  ## The input "specialmatrix" is a value that is to be obtained 
+  ## from the "makeCacheMatrix" function and will be the List of 4 functions.
+  ## i.e., if "x" is the matrix for which the inverse is to be computed,
+  ## specialmatrix should be "makeCacheMatrix(x)"
+  
+  i <- specialmatrix$getinv()
+  if(!is.null(i)) {
+    message("Retrieving the cached matrix data")
+    return(i)
+  }
+  data <- specialmatrix$get()
+  i <- solve(data, ...)
+  specialmatrix$setinv(i)
+  i
 }
